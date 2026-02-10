97c2c50fd9c595695d0085439e5488305b4916d6
cybersteve547 <minecraftstevso@gmail.com>
2026-02-09 19:34:09 -0500

    Add MINECON 2011 event details to events.ts

--- src/lib/editions/java/events.ts
+++ src/lib/editions/java/events.ts
@@ -13,5 +13,17 @@ export const events: Version[] = [
       "According to Notch, he sold Mojang because he didn't want the responsibility of owning a company of such global significance.",
       "As soon as Mojang was sold, Notch and the other two founders (Carl and Jakob) left the company."
     ]
+  },
+  {
+    title: "MINECON 2011",
+    type: "event",
+    date: "2011-11-18",
+    icon: "/event_microsoft.png",
+    learnMore: "https://minecraft.wiki/w/MINECON_2011",
+    longDescription: [
+      "MINECON 2011 was held at Mandalay Bay, Las Vegas, Nevada, USA on November 18th, coinciding with the official release of the game.[3] Tickets went on sale on August 11, 2011.",
+      "MINECON featured many exciting events, including the official release of Minecraft, keynote speeches from members of the Minecraft community (including one from Notch), building contests, breakout classes with different Minecraft topics, costume contests, exhibits, meeting worldwide Minecraft personalities and commemorative merchandise."
+      "Attendees received an exclusive MINECON 2011 cape, which depicts a creeper.""
+    ]
   }
 ] as const;
